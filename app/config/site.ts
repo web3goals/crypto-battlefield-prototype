135374e3851139375051e18062467bc72304370d
kiv1n <aloha@kiv1n.ru>
2024-04-27 10:01:47 +0300

    Implement landing page, config necessary dependencies

---
+++ app/config/site.ts
@@ -0,0 +1,33 @@
+import { Chain, scrollSepolia } from "viem/chains";
+
+export type SiteConfig = typeof siteConfig;
+
+export type SiteConfigContracts = {
+  chain: Chain;
+  battle: `0x${string}`;
+  entryPoint: `0x${string}`;
+  paymaster: `0x${string}`;
+  accountFactory: `0x${string}`;
+  accountAbstractionSuported: boolean;
+};
+
+export const siteConfig = {
+  emoji: "⚔️",
+  name: "Crypto Battlefield",
+  description:
+    "A crypto game based on your skills, ZK proofs and the price of tokens",
+  links: {
+    github: "https://github.com/web3goals/crypto-battlefield-prototype",
+  },
+  contracts: {
+    scrollSepolia: {
+      chain: scrollSepolia,
+      battle: "0x0000000000000000000000000000000000000000" as `0x${string}`,
+      entryPoint: "0x0000000000000000000000000000000000000000" as `0x${string}`,
+      paymaster: "0x0000000000000000000000000000000000000000" as `0x${string}`,
+      accountFactory:
+        "0x0000000000000000000000000000000000000000" as `0x${string}`,
+      accountAbstractionSuported: false,
+    } as SiteConfigContracts,
+  },
+};
