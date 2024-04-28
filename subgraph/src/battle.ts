cd28a6c1e6bf43267d46784910999d5bba8ca471
kiv1n <aloha@kiv1n.ru>
2024-04-28 09:25:24 +0300

    Implement subgraph functions for battle and user entities

--- subgraph/src/battle.ts
+++ subgraph/src/battle.ts
@@ -1,120 +1,105 @@
+import { Bytes } from "@graphprotocol/graph-ts";
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
   Ended as EndedEvent,
   Joined as JoinedEvent,
   Started as StartedEvent,
   Transfer as TransferEvent,
 } from "../generated/Battle/Battle";
-import {
-  Approval,
-  ApprovalForAll,
-  Ended,
-  Joined,
-  Started,
-  Transfer,
-} from "../generated/schema";
+import { Battle, Transfer, User } from "../generated/schema";
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.owner = event.params.owner;
-  entity.approved = event.params.approved;
-  entity.tokenId = event.params.tokenId;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.owner = event.params.owner;
-  entity.operator = event.params.operator;
-  entity.approved = event.params.approved;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-}
-
-export function handleEnded(event: EndedEvent): void {
-  let entity = new Ended(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.tokenId = event.params.tokenId;
-  entity.params_userOneSquad = event.params.params.userOneSquad;
-  entity.params_userOneSquadHash = event.params.params.userOneSquadHash;
-  entity.params_userTwo = event.params.params.userTwo;
-  entity.params_userTwoSquad = event.params.params.userTwoSquad;
-  entity.params_result = event.params.params.result;
-  entity.params_btcUsd = event.params.params.btcUsd;
-  entity.params_ethUsd = event.params.params.ethUsd;
-  entity.params_linkUsd = event.params.params.linkUsd;
-  entity.params_started = event.params.params.started;
-  entity.params_joined = event.params.params.joined;
-  entity.params_ended = event.params.params.ended;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
-}
-
-export function handleJoined(event: JoinedEvent): void {
-  let entity = new Joined(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.tokenId = event.params.tokenId;
-  entity.params_userOneSquad = event.params.params.userOneSquad;
-  entity.params_userOneSquadHash = event.params.params.userOneSquadHash;
-  entity.params_userTwo = event.params.params.userTwo;
-  entity.params_userTwoSquad = event.params.params.userTwoSquad;
-  entity.params_result = event.params.params.result;
-  entity.params_btcUsd = event.params.params.btcUsd;
-  entity.params_ethUsd = event.params.params.ethUsd;
-  entity.params_linkUsd = event.params.params.linkUsd;
-  entity.params_started = event.params.params.started;
-  entity.params_joined = event.params.params.joined;
-  entity.params_ended = event.params.params.ended;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-
-  entity.save();
+function loadOrCreateUser(id: Bytes): User {
+  let user = User.load(id);
+  if (!user) {
+    user = new User(id);
+    user.battles = 0;
+    user.victories = 0;
+    user.defeats = 0;
+    user.draws = 0;
+  }
+  return user;
 }
 
 export function handleStarted(event: StartedEvent): void {
-  let entity = new Started(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.tokenId = event.params.tokenId;
-  entity.params_userOneSquad = event.params.params.userOneSquad;
-  entity.params_userOneSquadHash = event.params.params.userOneSquadHash;
-  entity.params_userTwo = event.params.params.userTwo;
-  entity.params_userTwoSquad = event.params.params.userTwoSquad;
-  entity.params_result = event.params.params.result;
-  entity.params_btcUsd = event.params.params.btcUsd;
-  entity.params_ethUsd = event.params.params.ethUsd;
-  entity.params_linkUsd = event.params.params.linkUsd;
-  entity.params_started = event.params.params.started;
-  entity.params_joined = event.params.params.joined;
-  entity.params_ended = event.params.params.ended;
+  // Save battle
+  let battle = new Battle(event.params.tokenId.toString());
+  battle.userOne = event.transaction.from;
+  battle.userOneSquad = event.params.params.userOneSquad;
+  battle.userOneSquadHash = event.params.params.userOneSquadHash;
+  battle.userTwo = event.params.params.userTwo;
+  battle.userTwoSquad = event.params.params.userTwoSquad;
+  battle.result = event.params.params.result;
+  battle.btcUsd = event.params.params.btcUsd;
+  battle.ethUsd = event.params.params.ethUsd;
+  battle.linkUsd = event.params.params.linkUsd;
+  battle.started = event.params.params.started;
+  battle.joined = event.params.params.joined;
+  battle.ended = event.params.params.ended;
+  battle.save();
+  // Update user
+  let user = loadOrCreateUser(battle.userOne);
+  user.battles += 1;
+  user.save();
+}
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+export function handleJoined(event: JoinedEvent): void {
+  // Update battle
+  let battle = Battle.load(event.params.tokenId.toString());
+  if (!battle) {
+    return;
+  }
+  battle.userOne = event.transaction.from;
+  battle.userOneSquad = event.params.params.userOneSquad;
+  battle.userOneSquadHash = event.params.params.userOneSquadHash;
+  battle.userTwo = event.params.params.userTwo;
+  battle.userTwoSquad = event.params.params.userTwoSquad;
+  battle.result = event.params.params.result;
+  battle.btcUsd = event.params.params.btcUsd;
+  battle.ethUsd = event.params.params.ethUsd;
+  battle.linkUsd = event.params.params.linkUsd;
+  battle.started = event.params.params.started;
+  battle.joined = event.params.params.joined;
+  battle.ended = event.params.params.ended;
+  battle.save();
+  // Update user
+  let user = loadOrCreateUser(battle.userTwo);
+  user.battles += 1;
+  user.save();
+}
 
-  entity.save();
+export function handleEnded(event: EndedEvent): void {
+  // Update battle
+  let battle = Battle.load(event.params.tokenId.toString());
+  if (!battle) {
+    return;
+  }
+  battle.userOne = event.transaction.from;
+  battle.userOneSquad = event.params.params.userOneSquad;
+  battle.userOneSquadHash = event.params.params.userOneSquadHash;
+  battle.userTwo = event.params.params.userTwo;
+  battle.userTwoSquad = event.params.params.userTwoSquad;
+  battle.result = event.params.params.result;
+  battle.btcUsd = event.params.params.btcUsd;
+  battle.ethUsd = event.params.params.ethUsd;
+  battle.linkUsd = event.params.params.linkUsd;
+  battle.started = event.params.params.started;
+  battle.joined = event.params.params.joined;
+  battle.ended = event.params.params.ended;
+  battle.save();
+  // Update users
+  let userOne = loadOrCreateUser(battle.userOne);
+  let userTwo = loadOrCreateUser(battle.userTwo);
+  if (battle.result.toString() == "1") {
+    userOne.victories += 1;
+    userTwo.defeats += 1;
+  } else if (battle.result.toString() == "2") {
+    userOne.defeats += 1;
+    userTwo.victories += 1;
+  } else {
+    userOne.draws += 1;
+    userTwo.draws += 1;
+  }
+  userOne.save();
+  userTwo.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
